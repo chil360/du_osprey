b7aeaa999cd06071bd4c54fc08977ebeea0f662a
Kyle Harrison <khwebmail@gmail.com>
2018-09-15 18:47:31 +0100

    Add patch for HAL1 camcorder
    - This patches frameworks/av with the code to fix camcorder with the recent security patches

--- patch.sh
+++ patch.sh
@@ -5,3 +5,7 @@ BASEDIR=$(pwd)
 cd $BASEDIR/frameworks/base
 git am -3 ../../0001-Camera-allow-camera-to-use-power-key-as-shutter.patch
 cd $BASEDIR
+
+cd $BASEDIR/frameworks/av
+git am -3 ../../0001-OMXNodeInstance-Allow-dynamic-native-handle-for-shar.patch
+cd $BASEDIR
