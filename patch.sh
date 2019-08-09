3e7c8fce1a61f6c3aa0df11b159304d12f21ade4
Kyle Harrison <khwebmail@gmail.com>
2019-08-09 11:22:19 +0100

    Update to new lag fix patches

--- patch.sh
+++ patch.sh
@@ -72,8 +72,7 @@ git am -3 $TOP/0001-Improve-performance-of-unclipped-save-layers.patch
 cd $TOP
 
 cd external/skia
-git am -3 $TOP/0001-Add-rect-parameter-to-makeImageSnapshot-and-stop-usi.patch
-git am -3 $TOP/0002-fGpu-is-null-when-GrGpuResource-release.patch
+git am -3 $TOP/0001-Add-private-save-behind-and-draw-behind-methods-to-c.patch
 cd $TOP
 
 ##
